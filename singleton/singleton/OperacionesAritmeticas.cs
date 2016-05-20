a40c22d3850fde8693eba068cc23624a1342b209
boris.jramirez <boris.jramirez@hotmail.com>
2016-05-19 19:34:21 -0600

    primer merge

---
+++ singleton/singleton/OperacionesAritmeticas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    public class OperacionesAritmeticas
+    {
+        private static OperacionesAritmeticas operaciones = null;
+        private OperacionesAritmeticas() { }
+        public static OperacionesAritmeticas GetInstance()
+        {
+            if (operaciones == null)
+            {
+                operaciones = new OperacionesAritmeticas();
+            }
+            return operaciones;
+        }
+
+        public int sumar(int x, int y)
+        {
+            return x + y;
+        }
+        public int restar(int x, int y)
+        {
+            return x - y;
+        }
+        public int multiplicar(int x, int y)
+        {
+            return x * y;
+        }
+        public double dividir(double x, double y)
+        {
+            return x / y;
+        }
+    }
+
+}
---
+++ singleton/singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 2;
+            double a = 12, b = 5;
+
+            OperacionesAritmeticas operacion = OperacionesAritmeticas.GetInstance();
+
+            Console.WriteLine(string.Format("{0} + {1} = {2}", x, y, operacion.sumar(x, y)));
+            Console.WriteLine(string.Format("{0} - {1} = {2}", x, y, operacion.restar(x, y)));
+            Console.WriteLine(string.Format("{0} * {1} = {2}", x, y, operacion.multiplicar(x, y)));
+            Console.WriteLine(string.Format("{0} / {1} = {2}", x, y, operacion.dividir(a, b)));
+
+            Console.Read();
+        }
+    }
+}
